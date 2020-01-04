df550f91ae12351f0237c59f3fab28fc13da17d1
Devin34 <halld35n@gmail.com>
2020-01-03 21:14:33 -0600

    feat: added a Gallery section and sliding images
    Added temporary image and changed background color but its likly to change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  theme: {
-    fontFamily: {
-     'sans': ['-apple-system', 'BlinkMacSystemFont', ...],
-     'serif': ['Georgia', 'Cambria', ...],
-     'mono': ['SFMono-Regular', 'Menlo', ...],
-  },
+  theme: {},
   variants: {},
   plugins: []
 };
